59f24fd3f874d35278261c24113dc34d22fdc879
quistian241 <c7gonzalez@ucsd.edu>
2022-02-24 16:30:43 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -39,20 +39,20 @@ public class MarkdownParseTest {
     public void testSnippet1() throws IOException {
         String contents= Files.readString(Path.of("./snippet-1.md"));
         List<String> expect = List.of("`google.com");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet2() throws IOException {
         String contents= Files.readString(Path.of("./snippet-2.md"));
         List<String> expect = List.of("a.com", "a.com(())", "example.com");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet3() throws IOException {
         String contents= Files.readString(Path.of("./snippet-3.md"));
         List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 }
